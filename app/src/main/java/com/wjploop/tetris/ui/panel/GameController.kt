2b62cdb137bc0c2ee61bb53c02150130d317d1d0
wjploop <wjploop@163.com>
2021-06-10 14:56:05 +0800

    游戏按钮拟物效果

--- app/src/main/java/com/wjploop/tetris/ui/panel/GameController.kt
+++ app/src/main/java/com/wjploop/tetris/ui/panel/GameController.kt
@@ -1,40 +1,39 @@
 package com.wjploop.tetris.ui.panel
 
-import android.util.Log
 import android.view.MotionEvent
 import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.gestures.*
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
-import androidx.compose.material.ButtonDefaults.buttonColors
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.draw.rotate
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.draw.*
+import androidx.compose.ui.graphics.*
+import androidx.compose.ui.graphics.drawscope.inset
 import androidx.compose.ui.input.pointer.pointerInteropFilter
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.graphics.drawable.toBitmap
+import com.wjploop.tetris.R
 import com.wjploop.tetris.ext.logx
 import com.wjploop.tetris.ui.gamer.Gamer
 import com.wjploop.tetris.ui.gamer.LocalGamer
 import kotlinx.coroutines.*
-import kotlinx.coroutines.sync.Mutex
-import kotlin.math.log
 
-private val direction_button_size = 48.dp
-private val system_button_size = 28.dp
-private val spacer_height = 8.dp
+// 模拟按钮边缘阴影
+private val button_shadow_stroke_width = 6.dp
+
+private val direction_button_size = 48.dp + button_shadow_stroke_width * 2
+private val system_button_size = 28.dp + button_shadow_stroke_width * 2
+private val spacer_height = 8.dp + button_shadow_stroke_width * 2
+
 
 @Composable
 fun GameController() {
@@ -59,8 +58,6 @@ fun DirectionController(gamer: Gamer = LocalGamer.current) {
     Box(
         Modifier.size(direction_button_size * 3),
     ) {
-        // todo 支持长按发送命令
-        // todo 按钮实现拟物效果
         GameActionButton(Modifier.align(Alignment.TopCenter), onAction = { gamer.rotate() })
         GameActionButton(Modifier.align(Alignment.CenterEnd), onAction = { gamer.right() })
         GameActionButton(Modifier.align(Alignment.BottomCenter), onAction = { gamer.down(true) })
@@ -158,52 +155,64 @@ fun GameActionButton(
 
     val alpha: Float by animateFloatAsState(if (!isDown) 1f else 0.5f)
 
-
     val scope = rememberCoroutineScope()
     var longPressJob: Job? by remember {
         mutableStateOf(null)
     }
-
-    Surface(shape = CircleShape,
-        color = color,
-//        border = BorderStroke(2.dp, Color.Black),
-        content = {},
-        modifier = modifier
-            .pointerInteropFilter {
-                when (it.action) {
-                    MotionEvent.ACTION_DOWN -> {
-                        logx("action down")
-                        onAction()
-                        isDown = true
-                        longPressJob = scope.launch {
-                            delay(300)
-                            launch {
-                                while (isActive) {
-                                    delay(60)
-                                    logx("do action while is press")
-                                    onAction()
-                                }
+    val context = LocalContext.current
+    Canvas(modifier = modifier
+        .size(size = size)
+        .pointerInteropFilter {
+            when (it.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    logx("action down")
+                    onAction()
+                    isDown = true
+                    longPressJob = scope.launch {
+                        delay(300)
+                        launch {
+                            while (isActive) {
+                                delay(60)
+                                logx("do action while is press")
+                                onAction()
                             }
                         }
                     }
-                    MotionEvent.ACTION_UP -> {
-                        logx("action up")
-                        logx("current longPressJob? ${longPressJob}")
-                        longPressJob?.cancel()
-                        isDown = false
-                    }
-                    // not found trigger this event
-                    MotionEvent.ACTION_CANCEL -> {
-                        logx { "action cancel" }
-                        longPressJob?.cancel()
-                        isDown = false
-                    }
                 }
-                true
+                MotionEvent.ACTION_UP -> {
+                    logx("action up")
+                    logx("current longPressJob? ${longPressJob}")
+                    longPressJob?.cancel()
+                    isDown = false
+                }
+                // not found trigger this event
+                MotionEvent.ACTION_CANCEL -> {
+                    logx { "action cancel" }
+                    longPressJob?.cancel()
+                    isDown = false
+                }
             }
-            .alpha(alpha = alpha)
-            .size(size = size)
-    )
+            true
+        }
+    ) {
+        val shadowWidth = button_shadow_stroke_width.toPx()
+        // refer to https://stackoverflow.com/questions/5197892/add-shadow-to-custom-shape-on-android
+        drawImage(context.resources.getDrawable(R.drawable.game_btn_shadow).toBitmap(
+            width = size.toPx().toInt(),
+            height = size.toPx().toInt(),
+        ).asImageBitmap())
+
+        // draw shadow
+//        drawCircle(
+//            color = Color(0x20000000),
+//            style = Stroke(width = shadowWidth*2)
+//        )
+//         draw content
+        inset(shadowWidth / 2) {
+            drawCircle(color = color.copy(alpha = alpha))
+        }
+    }
+
 
 }
 
